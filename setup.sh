d754a17dfca5c3dd1606845e9f84916f6ce30ffa
aagrawa8 <aagrawa8@ncsu.edu>
2016-02-02 12:31:43 -0500

    save

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ if [ ! -d "ssh_keys" ]; then
   cp ~/.ssh/id_rsa.pub ssh_keys/id_rsa.pub
   # Control will enter here if $DIRECTORY doesn't exist.
 fi
+cp ~/.ssh/id_rsa ssh_keys/id_rsa
+cp ~/.ssh/id_rsa.pub ssh_keys/id_rsa.pub
 sudo apt-get update -y
 sudo apt-get install git
 
