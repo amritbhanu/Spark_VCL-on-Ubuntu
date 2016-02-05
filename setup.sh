22f6356a73a56e1e442274c3adf14a105f95babd
aagrawa8 <aagrawa8@ncsu.edu>
2016-02-05 16:51:15 -0500

    save

--- setup.sh
+++ setup.sh
@@ -1,8 +1,20 @@
 #from the source directory.
+sudo apt-get install python-pip
 wget https://pypi.python.org/packages/source/a/ansible/ansible-1.9.4.tar.gz
 tar -xvf ansible-1.9.4.tar.gz
 cd ansible-1.9.4
-python setup.py install
+sudo python setup.py install
+cd ..
+rm ansible-1.9.4.tar.gz
+sudo rm -rf ansible-1.9.4
+
+if [ ! -d "ssh_keys" ]; then
+  ssh-keygen -t rsa
+  mkdir ssh_keys
+  cp ~/.ssh/id_rsa ssh_keys/id_rsa
+  cp ~/.ssh/id_rsa.pub ssh_keys/id_rsa.pub
+  # Control will enter here if $DIRECTORY doesn't exist.
+fi
 
 sudo python setup.py install
 echo $1 > user.txt
