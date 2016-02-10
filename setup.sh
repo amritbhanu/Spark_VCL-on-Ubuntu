21fa4f4e94a0141fbbcb76d811f9d86cad962700
aagrawa8 <aagrawa8@ncsu.edu>
2016-02-09 21:36:18 -0500

    save

--- setup.sh
+++ setup.sh
@@ -8,13 +8,13 @@ cd ..
 rm ansible-1.9.4.tar.gz
 sudo rm -rf ansible-1.9.4
 
-if [ ! -d "ssh_keys" ]; then
+#if [ ! -d "ssh_keys" ]; then
   #ssh-keygen -t rsa
   #mkdir ssh_keys
   #cp ~/.ssh/id_rsa ssh_keys/id_rsa
   #cp ~/.ssh/id_rsa.pub ssh_keys/id_rsa.pub
   # Control will enter here if $DIRECTORY doesn't exist.
-fi
+#fi
 
 sudo python setup.py install
 echo $1 > user.txt
